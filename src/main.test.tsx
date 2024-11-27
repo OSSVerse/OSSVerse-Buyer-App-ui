89374e21f8947980464d8bf9baf727c40353c1b0
sudhir verma <sudhirvvermaa@gmail.com>
2024-11-27 16:59:56 +0530

    test ci

--- src/main.test.tsx
+++ src/main.test.tsx
@@ -6,6 +6,7 @@ import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 
 describe('Main Application', () => {
+
     it('renders without crashing', () => {
         const rootElement = document.createElement('div');
         rootElement.id = 'root';
